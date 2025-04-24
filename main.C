24e8bac7ed2f3c3eb7857ba566f6efee2bafc3c1
wendy743 <kerubu24.wendy@student.cuk.ac.ke>
2025-04-24 08:57:07 +0300

    Initial setup

---
+++ main.C
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// Functions
+float add(float a, float b);        // James 
+float subtract(float a, float b);   // James 
+float multiply(float a, float b);   // Yvette 
+float divide(float a, float b);     //  Brian 
+
+int main() {
+    float a = 10, b = 5;
+
+    printf("Addition: %.2f\n", add(a, b));
+    printf("Subtraction: %.2f\n", subtract(a, b));
+    printf("Multiplication: %.2f\n", multiply(a, b));
+    printf("Division: %.2f\n", divide(a, b));
+
+    return 0;
+}
+
+
+float add(float a, float b) {
+    // James's code here
+    return 0;
+}
+
+float subtract(float a, float b) {
+    //James 's code here
+    return 0; 
+}
+
+float multiply(float a, float b) {
+    // Yvette's code here
+    return 0;
+}
+
+float divide(float a, float b) {
+    // Brians's code here
+    return 0; 
+}
