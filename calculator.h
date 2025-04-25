b3dff19d992d54f53360077c63f6451356077d6f
OC11444 <james24.gitau@student.cuk.ac.ke>
2025-04-25 09:36:04 +0300

    solve redifinition error of divide

--- calculator.h
+++ calculator.h
@@ -1,28 +1,11 @@
-// calculator.c
-#include <stdio.h>
-#include "calculator.h"
+// calculator.h//wendy
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
 
-// Addition of  two numbers and return the result
-float add(float a, float b) {
-    return a + b;//james
-}
+// Function declarations
+float add(float a, float b);//james
+float subtract(float a, float b);//james
+float multiply(float a, float b);//mich
+float divide(float a, float b, int* error_flag);//wendy
 
-// Subtraction
-float subtract(float a, float b) {
-    return a - b;//james
-}
-
-// Multiplication of two values
-float multiply(float a, float b) {
-    return a * b;//yvtte
-}
-
-// Divisition of a by b, but checks for zero to avoid crash
-float divide(float a, float b, int* error_flag) {
-    if (b == 0) {
-        *error_flag = 1;  // marks an error that has happened either due touser iput
-        return 0.0;       //wendy//mich
-    }
-    *error_flag = 0;      // everything okay//.../james
-    return a / b;
-}
+#endif
